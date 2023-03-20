8d12412f4ac114297633ec7be5521007bd94c7c7
zxeinc <90633824+zxeinc@users.noreply.github.com>
2023-03-20 17:57:58 +0300

    init

---
+++ src/RecIntegral.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class RecIntegral {
+    public double start;
+    public double end;
+    public double step;
+    public double result ;
+
+    public RecIntegral(){
+
+    }
+
+    public RecIntegral(double _start, double _end, double _step){
+        start = _start;
+        end = _end;
+        step = _step;
+    }
+}
